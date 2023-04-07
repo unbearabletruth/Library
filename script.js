88b6d7b2a6799e0a30291d15402d34e3298c2dc5
unbearabletruth <antaarty@gmail.com>
2023-04-07 15:42:11 +0700

    Removed table; added form to add a new book.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let shelf = document.querySelector(".wrapper");
+let shelf = document.querySelector(".shelf");
+const button = document.querySelector(".addbutton");
+const inputtitle = document.querySelector("#title");
+const inputauthor = document.querySelector("#author");
+const inputpages = document.querySelector("#pages");
+const inputread = document.querySelector("#read");
 
 let myLibrary = [];
 
@@ -15,29 +20,61 @@ function addBookToLibrary(newbook) {
 
 function printBooks(){
     myLibrary.forEach((book)=>{
-        let newItem = document.createElement("tr"); 
+        let newItem = document.createElement("div"); 
         newItem.className = "book";
-        let newTitle = document.createElement("td");
-        let newAuthor = document.createElement("td");
-        let newPages = document.createElement("td");
-        let newRead = document.createElement("td");
+        let newTitle = document.createElement("div");
+        let newAuthor = document.createElement("div");
+        let newPages = document.createElement("div");
+        let newRead = document.createElement("div");
         newItem.appendChild(newTitle);
         newItem.appendChild(newAuthor);
         newItem.appendChild(newPages);
         newItem.appendChild(newRead);
         newTitle.innerText = `${book.name}`;
         newAuthor.innerText = `${book.author}`;
-        newPages.innerText = `${book.pages}`;
+        newPages.innerText = `Number of pages: ${book.pages}`;
         newRead.innerText = `${book.read}`;
         shelf.appendChild(newItem);
       })
 }
 
-const ThePlague = new Book("The Plague", "Albert Camus", 300, true);
+const ThePlague = new Book("The Plague", "Albert Camus", 300, "read");
 ThePlague 
-const NeverLetMeGo = new Book("Never Let Me Go", "Kazuo Ishiguro", 350, true);
+const NeverLetMeGo = new Book("Never Let Me Go", "Kazuo Ishiguro", 350, "read");
 addBookToLibrary(ThePlague);
-addBookToLibrary(NeverLetMeGo);
+//addBookToLibrary(NeverLetMeGo);
 
-printBooks();
+//printBooks();
 
+/*button.addEventListener("click", () => {
+    let form = document.createElement("form");
+    let titleinput = document.createElement("input");
+    titleinput.type = "text";
+    let authorinput = document.createElement("input");
+    authorinput.type = "text";
+    let pagesinput = document.createElement("input");
+    pagesinput.type = "number";
+    let readinput = document.createElement("input");
+    readinput.type = "text";
+    let submit = document.createElement("button");
+    form.appendChild(titleinput);
+    form.appendChild(authorinput);
+    form.appendChild(pagesinput);
+    form.appendChild(readinput);
+    form.appendChild(submit);
+    shelf.appendChild(form);
+    submit.addEventListener("click", () => {
+        const newbook = new Book(titleinput, authorinput, pagesinput, readinput);
+        console.log(newbook);
+        addBookToLibrary(newbook);
+    })
+}
+    );*/
+
+button.addEventListener("click", (e)=> {
+    const newbook = new Book(inputtitle.value, inputauthor.value, inputpages.value, inputread.value);
+    addBookToLibrary(newbook);
+    e.preventDefault();
+    printBooks();
+    
+})
